a8341d10b15300a45051e866f2ab1405d2401f2f
Mate Gergely <mate.gergely89@gmail.com>
2024-07-11 16:33:49 +0200

    upload the files

--- http_requests.py
+++ http_requests.py
@@ -1,9 +1,6 @@
 import requests
-import logging
 from constants import BASE_URL
 from data import user_data
-
-logging.basicConfig(level=logging.INFO)
 
 def get_request():
     response = requests.get(BASE_URL)
@@ -20,14 +17,14 @@ def post_request():
 
     if response.status_code == 201:
         new_user = response.json()
-        logging.info("\nÚj felhasználó sikeresen létrehozva:")
-        logging.info("Id: " + str(new_user['id']))
-        logging.info("Name: " + new_user['name'])
-        logging.info("Username: " + new_user['username'])
-        logging.info("Email: " + new_user['email'])
+        print("\nÚj felhasználó sikeresen létrehozva:")
+        print("Id: " + str(new_user['id']))
+        print("Name: " + new_user['name'])
+        print("Username: " + new_user['username'])
+        print("Email: " + new_user['email'])
         return new_user['id'] if new_user else None
     else:
-        logging.error("\nHiba történt a POST kérés során: " + str(response.status_code))
+        print("\nHiba történt a POST kérés során: " + str(response.status_code))
         return None
 
 def put_request(user_id):
@@ -48,14 +45,14 @@ def put_request(user_id):
 
     if response.status_code == 200:
         updated_user = response.json()
-        logging.info("\nFelhasználó frissítve:")
-        logging.info("Id: " + str(updated_user['id']))
-        logging.info("Name: " + updated_user['name'])
-        logging.info("Username: " + updated_user['username'])
-        logging.info("Email: " + updated_user['email'])
+        print("\nFelhasználó frissítve:")
+        print("Id: " + str(updated_user['id']))
+        print("Name: " + updated_user['name'])
+        print("Username: " + updated_user['username'])
+        print("Email: " + updated_user['email'])
     else:
-        logging.error("\nHiba történt a PUT kérés során: " + str(response.status_code))
-        logging.error(response.text)
+        print("\nHiba történt a PUT kérés során: " + str(response.status_code))
+        print(response.text)
 
 def delete_request(user_id):
     url = f"{BASE_URL}/{user_id}"
@@ -63,7 +60,7 @@ def delete_request(user_id):
     response = requests.delete(url)
 
     if response.status_code == 200:
-        logging.info(f"\nA(z) {user_id} azonosítójú felhasználó sikeresen törölve!")
+        print(f"\nA(z) {user_id} azonosítójú felhasználó sikeresen törölve!")
     else:
-        logging.error(f"\nHiba történt a DELETE kérés során az {user_id} azonosítóval: {response.status_code}")
-        logging.error(response.text)
+        print(f"\nHiba történt a DELETE kérés során az {user_id} azonosítóval: {response.status_code}")
+        print(response.text)
